324e7141fc4842819ba0d404c4ed308062dafdd0
SUJIT SWAIN <90376564+Sujitswain@users.noreply.github.com>
2023-09-21 11:54:23 +0530

    Update Game.js

--- components/Game.js
+++ components/Game.js
@@ -1,26 +1,90 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Die from './Die'
 import {nanoid} from "nanoid"
+import Confetti from "react-confetti";
 
 import './style.css'
 
 export default function Game()
 {
     const[dice, setDice] = React.useState(allNewDice())
-    const[rolls, setRolls] = React.useState(0)
     const[tenzies, settenzies] = React.useState(false)
 
+    const[rolls, setRolls] = React.useState(0)
+    const[bestRolls, setBestRolls] = React.useState(JSON.parse(localStorage.getItem("BestRolls")) || 0)
+    
+
+    // for setting current time
+    const[time, setTime] = React.useState(0)
+    const[running, setRunning] = React.useState(false)
+
+    // setting the best time
+    const[bestTime, setBestTime] = React.useState(JSON.parse(localStorage.getItem("BestTime")) || 0)
+
+    // start the timer
+    React.useEffect(()=>{
+        let interval;
+        if(running == true)
+        {
+            interval = setInterval(()=>{
+                setTime(prevTime => prevTime = prevTime + 10)
+            }, 10)
+        }
+        else if(!running)
+        {
+            clearInterval(interval)
+        }
+        return ()=>clearInterval(interval)
+
+    }, [running])
+    
     // check whether all dice are of same
     // number and also check that all dice
     // are selected
     React.useEffect(()=>{
 
+        const someHeld = dice.some(die => die.isHeld)
         const allHeld = dice.every(die => die.isHeld)
         const allSameValue = dice.every(die => die.value === dice[0].value)
-    
+        
+        if(someHeld)
+        {
+            setRunning(true)
+        }
+
         if(allHeld && allSameValue)
+        {
+            // stop timer
+            setRunning(false)
+
+            // update rolls
+            if(bestRolls == 0)
+            {
+                setBestRolls(rolls)
+                localStorage.setItem("BestRolls", JSON.stringify(bestRolls))
+            }
+            else if(rolls < bestRolls)
+            {
+                setBestRolls(rolls)
+                localStorage.setItem("BestRolls", JSON.stringify(bestRolls))
+            }
+
+            // update time
+            if(bestTime == 0)
+            {
+                setBestTime(time)
+                localStorage.setItem("BestTime", JSON.stringify(time))
+            }
+            else if(time < bestTime)
+            {
+                setBestTime(time)
+                localStorage.setItem("BestTime", JSON.stringify(time))
+            }
+
+            // win
             settenzies(true)
-    }, [dice])
+        }
+    }, [dice, time, bestTime])
 
     // generating the object 
     // with all necessary fields
@@ -64,6 +128,7 @@ export default function Game()
         }
         else
         {
+            setTime(0)
             setRolls(0)
             settenzies(false)
             setDice(allNewDice())
@@ -97,6 +162,7 @@ export default function Game()
 
     return(
        <main>
+            {tenzies && <Confetti />}
             <h1 className="title">Tenzies</h1>
             
             <p className="instructions">
@@ -104,6 +170,25 @@ export default function Game()
                 die to freeze it at its current value between
                 rolls
             </p>
+            
+            <div className="best-score-info">
+                <span>
+                    Best Rolls: {bestRolls}
+                </span>
+                <span>
+                    Best Time:      
+                    {
+                        ("0" + Math.floor((bestTime/60000) % 60)).slice(-2)
+                    }:
+                    {
+                        ("0" + Math.floor((bestTime/1000) % 60)).slice(-2)
+                    }:
+                    {
+                        ("0" + (bestTime/10) % 100).slice(-2)
+                    }
+                </span>
+            </div>
+
             { !tenzies 
                 ?  
                     <>
@@ -111,12 +196,40 @@ export default function Game()
                             {diceElements}
                         </div> 
 
-                        <span>Number of rolls: {rolls}</span>
+                        <div className="current-info">
+                            <span>
+                                Number of rolls: {rolls}
+                            </span>
+                            <span>
+                                Time: 
+                                {
+                                    ("0" + Math.floor((time/60000) % 60)).slice(-2)
+                                }:
+                                {
+                                    ("0" + Math.floor((time/1000) % 60)).slice(-2)
+                                }:
+                                {
+                                    ("0" + (time/10) % 100).slice(-2)
+                                }
+                            </span>
+                        </div>
                     </>
                 : 
                     <>
                         <h1>🙌 You Won 🙌</h1> 
                         <h3>Rolls : {rolls}</h3>
+                        <h3>
+                                Time: 
+                                {
+                                    ("0" + Math.floor((time/60000) % 60)).slice(-2)
+                                }:
+                                {
+                                    ("0" + Math.floor((time/1000) % 60)).slice(-2)
+                                }:
+                                {
+                                    ("0" + (time/10) % 100).slice(-2)
+                                }
+                        </h3>
                     </>
             }
             
